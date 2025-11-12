dc709d47f35aae64773f738653dfa5b08798fa60
Mario <mario.guenzel@tu-dortmund.de>
2025-11-12 10:34:34 +0100

    automatic evaluation

--- automatic_eval.sh
+++ automatic_eval.sh
@@ -1,3 +1,6 @@
+NUM_ITERATIONS=10  # number of runs for the time measurements of the synthetic evaluation
+REPEATING_RUNS=$((NUM_ITERATIONS - 1))
+
 # Benchmarks
 echo "== Evaluate Benchmarks =="
 
@@ -8,7 +11,7 @@ elapsed=$(echo "$end_time - $start_time" | bc)
 
 echo "Evaluation results stored in: results/case_studies.jsonl"
 
-echo "- Total elapsed time: ${elapsed}s"
+echo "- Total elapsed time for case studies: ${elapsed}s"
 python3 -c "import json; total = sum(json.loads(line).get('analysis_time_sec', 0) for line in open('results/case_studies.jsonl')); print(f'- SUM analysis_time_sec: {total}')"
 
 # python3 -c "import json; anchors = [(json.loads(line).get('ID', []), json.loads(line).get('#AnchorsRT', [])) for line in open('results/case_studies.jsonl')]; print('Anchor points:', anchors)"
@@ -22,57 +25,66 @@ echo "Table of Case-Study results stored in: paperdata/case_studies.tex"
 
 # Synthetic Evaluation - WATERS
 echo "== Synthetic Evaluation - WATERS Benchmark =="
+start_time=$(date +%s.%N)
 
-python3 generate.py --bench WATERS --tasks 5 --sets 100 --startid 0 EvalWATERS/chains05.jsonl
-python3 generate.py --bench WATERS --tasks 20 --sets 100 --startid 1000 EvalWATERS/chains20.jsonl
-python3 generate.py --bench WATERS --tasks 40 --sets 100 --startid 2000 EvalWATERS/chains50.jsonl
+python3 generate.py --bench WATERS --tasks 5 --sets 1000 --startid 0 EvalWATERS/chains05.jsonl
+python3 generate.py --bench WATERS --tasks 20 --sets 1000 --startid 10000 EvalWATERS/chains20.jsonl
+python3 generate.py --bench WATERS --tasks 40 --sets 1000 --startid 20000 EvalWATERS/chains50.jsonl
 
 echo "Chains stored in EvalWATERS/chains<tasks-per-set>.jsonl"
 
-for i in {0..2}; do
+for i in $(seq 0 $((NUM_ITERATIONS - 1))); do
     python3 analysis.py EvalWATERS/chains05.jsonl --info --relative-bound 0.95 --no-print --output EvalWATERS/results05_${i}.jsonl
 done
 
-for i in {0..2}; do
+for i in $(seq 0 $((NUM_ITERATIONS - 1))); do
     python3 analysis.py EvalWATERS/chains20.jsonl --info --relative-bound 0.95 --no-print --output EvalWATERS/results20_${i}.jsonl
 done
 
-for i in {0..2}; do
+for i in $(seq 0 $((NUM_ITERATIONS - 1))); do
     python3 analysis.py EvalWATERS/chains50.jsonl --info --relative-bound 0.95 --no-print --output EvalWATERS/results50_${i}.jsonl
 done
 
 echo "Analysis stored in EvalWATERS/results<tasks-per-set>.jsonl"
 
-python plot.py -o paperdata/WATERS_runtime.png paperdata/WATERS_anchors.png -c 3 EvalWATERS/results50_{0..2}.jsonl  EvalWATERS/results20_{0..2}.jsonl  EvalWATERS/results05_{0..2}.jsonl
+eval python plot.py -o paperdata/WATERS_runtime.png paperdata/WATERS_anchors.png -c $NUM_ITERATIONS EvalWATERS/results50_{0..$REPEATING_RUNS}.jsonl  EvalWATERS/results20_{0..$REPEATING_RUNS}.jsonl  EvalWATERS/results05_{0..$REPEATING_RUNS}.jsonl
 
 echo "Plots stored in paperdata/"
+
+end_time=$(date +%s.%N)
+elapsed=$(echo "$end_time - $start_time" | bc)
+echo "- Total elapsed time for synthetic evaluation with WATERS benchmark: ${elapsed}s"
 
 
 # Synthetic Evaluation - UNI 
 echo "== Synthetic Evaluation - UNIFORM Benchmark =="
+start_time=$(date +%s.%N)
 
-python3 generate.py --bench UNI --tasks 5 --sets 100 --startid 0 EvalUNI/chains05.jsonl --maxHTp 350
-python3 generate.py --bench UNI --tasks 20 --sets 100 --startid 1000 EvalUNI/chains20.jsonl --maxHTp 350
-python3 generate.py --bench UNI --tasks 40 --sets 100 --startid 2000 EvalUNI/chains50.jsonl --maxHTp 350
+python3 generate.py --bench UNI --tasks 5 --sets 1000 --startid 0 EvalUNI/chains05.jsonl --maxHTp 350
+python3 generate.py --bench UNI --tasks 20 --sets 1000 --startid 10000 EvalUNI/chains20.jsonl --maxHTp 350
+python3 generate.py --bench UNI --tasks 40 --sets 1000 --startid 20000 EvalUNI/chains50.jsonl --maxHTp 350
 
 echo "Chains stored in EvalUNI/chains<tasks-per-set>.jsonl"
 
-for i in {0..2}; do
+for i in $(seq 0 $((NUM_ITERATIONS - 1))); do
     python3 analysis.py EvalUNI/chains05.jsonl --info --relative-bound 0.95 --no-print --output EvalUNI/results05_${i}.jsonl
 done
 
-for i in {0..2}; do
+for i in $(seq 0 $((NUM_ITERATIONS - 1))); do
     python3 analysis.py EvalUNI/chains20.jsonl --info --relative-bound 0.95 --no-print --output EvalUNI/results20_${i}.jsonl
 done
 
-for i in {0..2}; do
+
+for i in $(seq 0 $((NUM_ITERATIONS - 1))); do
     python3 analysis.py EvalUNI/chains50.jsonl --info --relative-bound 0.95 --no-print --output EvalUNI/results50_${i}.jsonl
 done
 
 echo "Analysis stored in EvalUNI/results<tasks-per-set>.jsonl"
 
-python plot.py -o paperdata/UNI_runtime.png paperdata/UNI_anchors.png -c 3 EvalUNI/results50_{0..2}.jsonl  EvalUNI/results20_{0..2}.jsonl  EvalUNI/results05_{0..2}.jsonl
+eval python plot.py -o paperdata/UNI_runtime.png paperdata/UNI_anchors.png -c $NUM_ITERATIONS EvalUNI/results50_{0..$REPEATING_RUNS}.jsonl  EvalUNI/results20_{0..$REPEATING_RUNS}.jsonl  EvalUNI/results05_{0..$REPEATING_RUNS}.jsonl
 
 echo "Plots stored in paperdata/"
 
-
+end_time=$(date +%s.%N)
+elapsed=$(echo "$end_time - $start_time" | bc)
+echo "- Total elapsed time for synthetic evaluation with UNIFORM benchmark: ${elapsed}s"
