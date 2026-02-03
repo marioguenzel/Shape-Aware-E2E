8558528f49ae52d7f214f671476233f72bdf95ba
Mario <mario.guenzel@tu-dortmund.de>
2026-02-03 17:45:23 +0100

    start implementing the shepherding suggested approach

--- compare_methods.py
+++ compare_methods.py
@@ -2,6 +2,7 @@
 
 import argparse
 import itertools
+import time
 from analysis import Task as OurTask
 from analysis import CEChain as OurCEChain
 from analysis import load_chains_from_jsonl
@@ -302,14 +303,6 @@ class PartitionedJobChain:
 
 # Analyses
 
-def hamann17(chain):
-    """https://drops.dagstuhl.de/entities/document/10.4230/LIPIcs.ECRTS.2017.10"""
-    latency = 0
-    for task in chain:
-        latency += task.max_iat + task.deadline
-    return latency
-
-
 def _release_after(time, task):
     """Next release of task at or after 'time' for periodic tasks."""
     return task.phase + math.ceil((time - task.phase) / task.period) * task.period
@@ -339,7 +332,8 @@ def LET_per(chain):
         relvar = _release(mvar + 1, chain[0])
 
         # check conditions
-        if relvar + chain.base_ts.wcrts[chain[0]] < max_phase:
+        # if relvar + chain.base_ts.wcrts[chain[0]] < max_phase:
+        if relvar + chain[0].deadline < max_phase:
             continue
         if zvar > max_phase + hyper + WCRT_max:
             break
@@ -356,9 +350,136 @@ def LET_per(chain):
 
     return max(lengths)
 
-# TODO: Partitioned job chains
+# === Partitioned job chains ===
+# https://github.com/tu-dortmund-ls12-rt/mrt_mda/
+
+def let_we(job: Job):
+    """Write-event of job under LET."""
+    return job.task.phase + job.task.period * job.occurrence + job.task.deadline
+
+def let_re(job: Job):
+    """Read-event of job under LET."""
+    return job.task.phase + job.task.period * job.occurrence
+
+def let_re_geq(time, task: Task):
+    """Number of earliest job with read-event of (task) at or after (time)."""
+    return max(math.ceil((time - task.phase) / task.period), 0)
+
+def let_re_gt(time, task: Task):
+    """Number of earliest job with read-event of (task) after (time)."""
+    return max(math.floor((time - task.phase) / task.period) + 1, 0)
+
+def let_we_leq(time, task: Task):
+    """Number of latest job with write-event of (task) at or before (time)."""
+    return math.floor((time - task.phase - task.deadline) / task.period)
+
+def get_fw_jobchain(ce_chain: CEChain, occurrence: int):
+    """Create (occurrence)-th immediate forward job chain. (under implicit comm.)"""
+
+    # first job
+    job_lst = [Job(ce_chain[0], occurrence)]
+
+    # next jobs
+    for tsk in ce_chain[1:]:
+        # find next job
+        next_job_occurrence = let_re_geq(let_we(job_lst[-1]), tsk)
+
+        # add job to chain
+        job_lst.append(Job(tsk, next_job_occurrence))
+        
+    return JobChain(*job_lst)
+
+
+def get_bw_jobchain(ce_chain: CEChain, occurrence: int):
+    """Create (occurrence)-th immediate backward job chain. (under implicit comm.)"""
+
+    # last job
+    job_lst = [Job(ce_chain[-1], occurrence)]
+
+    # previous jobs jobs
+    for tsk in ce_chain[0:-1][::-1]:  # backwards except the last
+        # find previous job
+        previous_job_occurrence = let_we_leq(let_re(job_lst[0]), tsk)
+
+        # add job to chain
+        job_lst.insert(0, Job(tsk, previous_job_occurrence))
+    
+    return JobChain(*job_lst)
+
+
+def get_part_jobchain(part, chain, occurrence):
+    """Create a partitioned job chain.
+    - part = where is the partioning
+    - chain = cause-effect chain
+    - occurrence = which chain
+    - ana = analyzer for read/writes under implicit comm."""
+
+    bw = get_bw_jobchain(CEChain(*chain[: part + 1], base_ts=chain.base_ts), occurrence)
+    fw = get_fw_jobchain(CEChain(*chain[part:], base_ts=chain.base_ts), occurrence + 1)
+
+    return PartitionedJobChain(chain, fw, bw)
+
+
+def ell(pc : PartitionedJobChain):
+    """Length of the partitioned job chain, more precisely l() function from the paper."""
+    return let_we(pc.fw[-1]) - let_re(pc.bw[0])
+
+
+def find_fi(ce_chain: CEChain) -> list[int]:
+    """List of Fi values."""
+    # one forward chain
+    fc = get_fw_jobchain(ce_chain, 0)
+    F = fc[-1].occurrence
+    # one backward chain
+    bc = get_bw_jobchain(ce_chain, F)
+
+    Fi = [job.occurrence for job in bc]
+    return Fi
+
+def guenzel23_equi_mda(chain: CEChain, Fi=None) -> float:
+    """Compute MRT or MDA as in our paper 
+    - optimized p for periodic synchrnous let tasks
+    """
+    # Construct F_i
+    if Fi is None:
+        Fi = find_fi(chain)
+
+    # find analysis interval
+    analysis_end = 2 * chain.hyperperiod() + chain.max_phase()
+
+    # choose point for partitioning
+    # for synchronous let just choose the task with highest period
+    periods = [tsk.period for tsk in chain]
+    part = periods.index(max(periods))
+
+    # construct partitioned chains
+    part_chains = []
+    for occurrence in itertools.count(start=Fi[part]):
+        pc = get_part_jobchain(part, chain, occurrence)
+        if let_re(pc.bw[0]) <= analysis_end:
+            part_chains.append(pc)
+        else:
+            break
+
+    assert all(pc.complete for pc in part_chains)
+    return max([ell(pc) for pc in part_chains])
+
+def guenzel23_equi_mrt(chain: CEChain, Fi=None) -> float:
+    return guenzel23_equi_mda(chain, Fi)
 
 # TODO: Approach Suggested by the shepherd
+def sun23(chain: CEChain) -> float:
+    """https://ieeexplore.ieee.org/document/10155700"""
+    """Algorithm 3"""
+    hyperperiod = ch.hyperperiod()
+    rel_last_job = ch[-1].phase
+    while True:
+        z = rel_last_job
+        
+        # Track data backwards
+        # TODO: Finish implementation here
+
+
 
 # Main
 
@@ -392,6 +513,31 @@ if __name__ == "__main__":
                 priority=None
                 ))
         translated_chains.append(CEChain(*new_task_list))
+        translated_chains[-1].id = ch.id
     
+
+    # Rund experiments
+    results = []
+    for ch in translated_chains:
+        this_chain_results = dict()
+        this_chain_results["ID"] = ch.id
+
+        # Based on forward job chains
+        start_time = time.time()
+        res = LET_per(ch)
+        end_time = time.time()
+        this_chain_results['FW_MRT'] = res
+        this_chain_results['FW_TIME'] = end_time - start_time
+
+        # Based on partitioned job chains
+        start_time = time.time()
+        res = guenzel23_equi_mrt(ch)
+        end_time = time.time()
+        this_chain_results['P_MRT'] = res
+        this_chain_results['P_TIME'] = end_time - start_time
+
+        results.append(this_chain_results)
+
+
     breakpoint()
 
